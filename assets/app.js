a4e549692d31e6c19e54574d3faa7a0ca30b2461
AntoineLudwigGalli <antoineludwiggalli@gmail.com>
2022-12-09 11:37:00 +0100

    reorganisation css et js avec webpack encore

--- assets/app.js
+++ assets/app.js
@@ -44,3 +44,16 @@ if (slider) {
     })
 }
 
+// Owl Carrousel settings
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        center: true,
+        nav: true,
+        autoplay: true,
+        lazyLoad: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 3000,
+    });
+});
